e1540964b39b6040e511656761dc26433f883e21
zyfdegh <zyfdegg@gmail.com>
2016-08-21 22:47:15 +0800

    edit travis.yml

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t hiupdate .
-docker run --rm -p 8080:8080 -v /tmp/hiupdate:/usr/local/bin hiupdate
+sudo docker build -t hiupdate .
+sudo docker run --rm -p 8080:8080 hiupdate
 
